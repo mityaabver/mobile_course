03a7ddfcaadd93a8bccb36a92b079639d026315b
averyanovda <averyanovda@corp.dev.vtb>
2022-11-14 00:14:25 +0300

    Test2

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -8,4 +8,10 @@ public class MainClassTest extends MainClass{
         int localNumber = getLocalNumber();
         Assert.assertTrue("метод getLocalNumber() вернул не 14", localNumber == 14);
     }
+
+    @Test //Test2
+    public void testGetClassNumber() {
+        int classNumber = getClassNumber();
+        Assert.assertTrue("метод getClassNumber() возвращает результат не больше 45", classNumber > 45);
+    }
 }
